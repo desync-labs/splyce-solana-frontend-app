b7c3ee65a11284d6577ea0dbb0a42a10a604275c
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2024-10-06 01:49:47 +0300

    vault description

--- src/utils/Vaults/getVaultTitleAndDescription.tsx
+++ src/utils/Vaults/getVaultTitleAndDescription.tsx
@@ -232,6 +232,59 @@ vaultDescription['1'.toLowerCase()] = (
   </>
 )
 
-vaultTitle['1'.toLowerCase()] = 'Gold World ETF'
+vaultDescription['11111111'.toLowerCase()] = (
+  <>
+    <VaultAboutTitle>
+      Gold World ETF Vault: Bridging Traditional Assets and Decentralized
+      Finance
+    </VaultAboutTitle>
+    <p>
+      Welcome to the Gold World ETF Vault, a pioneering real-world asset (RWA)
+      investment platform that seamlessly integrates the stability of
+      traditional asset investments with the innovation of decentralized
+      finance. This vault offers a unique opportunity for users to diversify
+      their portfolios by investing in precious commodities like gold,
+      delivering both security and profitability.
+    </p>
+    <b>Key Features:</b>
+    <DescriptionList>
+      <li>
+        Direct Investment in RWAs: Users can directly invest their funds into a
+        curated selection of real-world assets, primarily gold, known for its
+        enduring value and stability.
+      </li>
+      <li>
+        Automated Returns Distribution: Leveraging advanced algorithms, the
+        vault redistributes generated interest back to the users, ensuring a
+        passive income stream.
+      </li>
+      <li>
+        High Transparency and Security: Each investment is meticulously recorded
+        and audited, with a clear tracking mechanism that ensures full
+        transparency and security of the assets.
+      </li>
+      <li>
+        Performance-Based Fee Structure: No management fees are charged. A
+        performance fee is only applied when profits are realized, aligning our
+        interests with those of our investors.
+      </li>
+      <li>
+        Risk Mitigation: While investments in RWAs reduce exposure to volatile
+        digital assets, they still carry risks. Our platform uses stringent risk
+        assessment protocols to mitigate these, ensuring your investments are as
+        safe as possible.
+      </li>
+    </DescriptionList>
+    <p>
+      By participating in the Gold World ETF Vault, investors gain access to a
+      traditionally exclusive market through a decentralized platform, enjoying
+      the benefits of both worlds without the typical barriers to entry. Invest
+      with confidence and watch your digital and traditional assets grow
+      together.
+    </p>
+  </>
+)
+
+vaultTitle['11111111'.toLowerCase()] = 'Gold World ETF'
 
 export { vaultTitle, vaultDescription }
